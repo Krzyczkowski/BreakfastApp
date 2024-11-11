52cf4ea4ec3d3576b2540eb075ec84f45c1de999
krzyczkowski <krzyczkowski.wiktor00@gmail.com>
2024-11-11 09:53:48 +0100

    added Contracts
    Contracts are definition for input and output in my API interface.

--- BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
+++ BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -1,3 +1,10 @@
 namespace BuberBreakfast.Contracts.Breakfast;
 
-public record 
+public record CreateBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,   
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet
+);
